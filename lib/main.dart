e6155275d69009eb21142e08f3871b8272a8e9bd
Abhin J Gomez <adr24cs005@cea.ac.in>
2025-09-10 22:10:06 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fW54XeWoC_4hHOyWoIinBh6OVVkGX317vQ&s'),
+              // backgroundColor: Colors.red,
+              radius:60,
+            ),
+
+            Text("Abhin J Gomez",style: TextStyle(fontSize:25,color:Colors.white,fontWeight: FontWeight.bold),),
+            Text("FLUTTER DEVELOPER",style: TextStyle(fontSize:20,color:Colors.white,letterSpacing: 2),),
+            Padding(
+              padding: const EdgeInsets.only(left:100,right:100),
+              child: Divider(
+                color: Colors.white,
+                thickness: 1,
+                /*indent: 100,
+                endIndent: 100,*/
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                color:Colors.white,
+                height:40,
+                child:Row(
+                  children: [
+                    Icon(Icons.phone,color:Colors.teal,size: 25,),
+                    SizedBox(width:20),
+                    Text("+91 7306817426",style: TextStyle(fontSize:20,color:Colors.teal,fontWeight: FontWeight.bold),)
+                  ],
+                )
+              ),
+            ),
+            SizedBox(height:20),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                color:Colors.white,
+                height:40,
+                child:Row(
+                  children: [
+                    Icon( Icons.email,color:Colors.teal,size:25,),
+                    SizedBox(width:10),
+                    Text("abhinjgomez@gmail.com",style: TextStyle(fontSize:20,color:Colors.teal,fontWeight: FontWeight.bold),)
+                  ],
+                )
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
